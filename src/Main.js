4053cf53896e17b3a040fe07e34231cbee0f0ada
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2023-02-06 00:11:02 +0800

    Implemented example of the problem

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <main>
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ullam. Iure reiciendis aliquam, eaque eveniet ducimus, dolor facere distinctio odio labore ratione fugiat cupiditate laborum ex consequatur tempore quam ab.</p>
+    </main>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Main from './Main'
 
 function App() {
-  return (
-    <>
-      <p>
-        Hi! Need help with React Testing Library? The best way to get it is by
-        forking this repo, making a reproduction of your issue (or
-        showing what you're trying to do), and posting a link to it on our
-        Discord chat:
-      </p>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://testing-library.com/discord"
-      >
-        testing-library.com/discord
-      </a>
-    </>
-  )
+  return <Main />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
